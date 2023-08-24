b872e7f4dafa35239ac0d55924e58a3b92537f60
ngobenihc <ngobenihc@yahoo.com>
2023-08-24 16:59:47 +0200

    first commit

---
+++ lib/controllers/q_papers/up_load_data.dart
@@ -0,0 +1,14 @@
+import 'package:get/get.dart';
+
+class DataLoad extends GetxController {
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    upLoadData();
+    super.onReady();
+  }
+
+  void upLoadData() {
+    print("UpLoading dat........");
+  }
+}
